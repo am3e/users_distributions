019152446789024ba60fcd2e0ec9a1ede97ca867
amee <amalia.reodica@gmail.com>
2020-11-12 10:44:55 -0500

    test change

--- src/variables.js
+++ src/variables.js
@@ -1,7 +1,7 @@
 
 //Change for unassigned users
-const daysOutFree = 10;
-const daysOutBonus = 5;
+const daysOutFree = 260;
+const daysOutBonus = 6;
 
 //AUM threshold
 const AUM_BUCKET = 125000;
@@ -13,7 +13,7 @@ const aumLessThan = (aum, amount) => {
   return aum <= amount;
 }
 const aumComparisonType = aumLessThan;
-const aumUserCount = 4;
+const aumUserCount = 6;
 
 //distance of user to advisor
 const tier1 = 25;
@@ -22,8 +22,8 @@ const tier3 = 100;
 const tier4 = 275;
 
 //percentage of users distances to advisor
-const pctTier1 = 0.65;
-const pctTier2 = 0.45;
+const pctTier1 = 0.35;
+const pctTier2 = 0.55;
 const pctTier3 = 0.55;
 const pctTier4 = 0.75;
 const pctTier5 = 1.00;
